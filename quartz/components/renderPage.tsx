aa223b54689953c2ad724b9691aa7db9426860b8
msd98bsh <masoudgeek@gmail.com>
2026-01-03 12:05:39 +0330

    Quartz sync: Jan 3, 2026, 12:05 PM

--- quartz/components/renderPage.tsx
+++ quartz/components/renderPage.tsx
@@ -260,7 +260,7 @@ export function renderPage(
   const lang = componentData.fileData.frontmatter?.lang ?? cfg.locale?.split("-")[0] ?? "en"
   const direction = i18n(cfg.locale).direction ?? "ltr"
   const doc = (
-    <html lang={lang} dir={direction}>
+    <html dir="rtl" lang={lang}>
       <Head {...componentData} />
       <body data-slug={slug}>
         <div id="quartz-root" class="page">
